e032a5ff9cad1ad31e83460c9cf2525fb47c9bc2
ihuanglele <huanglele@yousuowei.cn>
2019-01-25 14:57:10 +0800

    添加系统执行记录

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -16,14 +16,29 @@ class Bootstrap extends \Yaf\Bootstrap_Abstract {
 
     public function _initApp()
     {
-        $log    = Container::getConfig('log', 'fw\\logger\\File');
-        $logger = Logger::setLogger($log);
+        $t_start = microtime(true);
+        $log     = Container::getConfig('log', 'fw\\logger\\File');
+        $logger  = Logger::setLogger($log);
         Container::set(Container::SYSLOG, $logger);
         $cache    = Container::getConfig('cache');
         $cacheIns = Cache::setDriver($cache);
         Container::set(Container::SYSCACHE, $cacheIns);
-        register_shutdown_function(function()
+        register_shutdown_function(function() use ($t_start)
         {
+            $t_end     = microtime(true);
+            $mem_usage = memory_get_usage(true);
+            if ($mem_usage < 1024) {
+                $mem_usage .= " bytes";
+            } elseif ($mem_usage < 1048576) {
+                $mem_usage = round($mem_usage / 1024, 2)." KB";
+            } else {
+                $mem_usage = round($mem_usage / 1048576, 2)." M";
+            }
+            $info = [
+                'time'   => round($t_end - $t_start, 4),
+                'memory' => $mem_usage,
+            ];
+            Container::getLogger()->info('runtime', $info);
             Container::getLogger()->write();
         });
         set_error_handler(['fw\\ErrorHandle', 'appError']);
